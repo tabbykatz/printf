8e61a970e9c19fa7504b4937cc0fc68d891076af
Cynthia Taylor <820@holbertonschool.com>
2020-03-18 00:22:53 +0000

    Attempt to fix Valgrind warning

--- print_rot13.c
+++ print_rot13.c
@@ -12,10 +12,10 @@ int rot13(va_list args)
 	char *str = va_arg(args, char *);
 	char *rot13 = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 	char *alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-	int idx, count;
-	char *encoded;
+	int idx, count = 0;
+	char *encoded = NULL;
 
-	encoded = malloc(sizeof(str));
+	encoded = malloc(sizeof(char) * (_strlen(str) + 1));
 
 	if (!encoded || !str)
 		return (-1);
@@ -30,6 +30,7 @@ int rot13(va_list args)
 				break;
 			}
 		}
+/* Insert characters that do not get rot13'd -- punctuation and numbers */
 		if (str[count] != alphabet[idx])
 			encoded[count] = str[count];
 	}
